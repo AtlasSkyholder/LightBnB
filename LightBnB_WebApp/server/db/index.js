986fefa86048ebe78e00dd8ca534cef1445482a2
Fred <thecanuckatheist@gmail.com>
2019-11-15 01:10:09 +0000

    fix search engine mistake

--- LightBnB_WebApp/server/db/index.js
+++ LightBnB_WebApp/server/db/index.js
@@ -1,5 +1,5 @@
-const properties = require('../json/properties.json');
-const users = require('../json/users.json');
+/* const properties = require('../json/properties.json');
+const users = require('../json/users.json'); */
 const { Pool } = require('pg');
 
 const pool = new Pool({
@@ -114,7 +114,7 @@ const getAllProperties = function(options, limit = 10) {
   let queryString = `
   SELECT properties.*, avg(property_reviews.rating) as average_rating
   FROM properties
-  JOIN property_reviews ON properties.id = property_id
+  LEFT JOIN property_reviews ON properties.id = property_id
   `;
 
   // 3
@@ -163,6 +163,7 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
+  console.log(property);
   return pool.query(`
   INSERT INTO properties (owner_id, title, description, thumbnail_photo_url, cover_photo_url, cost_per_night, street, city, province, post_code, country, parking_spaces, number_of_bathrooms, number_of_bedrooms)
   VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
@@ -170,5 +171,5 @@ const addProperty = function(property) {
   `, [property.owner_id, property.title, property.description, property.thumbnail_photo_url, property.cover_photo_url, property.cost_per_night, property.street, property.city, property.province, property.post_code, property.country, property.parking_spaces, property.number_of_bathrooms, property.number_of_bedrooms]).then(res => {
     return (res.rows.length > 0) ? res.rows[0] : null;
   });
-}
+};
 exports.addProperty = addProperty;
